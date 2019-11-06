01a088b5d8445c8ac37dfbb57de0de94138bda07
Troy Christian <apostasy8@gmail.com>
2019-11-06 01:21:51 -0500

    TableView StoryBoard file complete

---
+++ GrailProject/GrailProject/POIsTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  POIsTableViewController.swift
+//  GrailProject
+//
+//  Created by Lambda_School_Loaner_219 on 11/5/19.
+//  Copyright © 2019 Lambda_School_Loaner_219. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
+
+    
